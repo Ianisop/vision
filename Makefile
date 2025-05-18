53e3ac16d290aee2c1fc884724316fcb54ac4f5f
SomethingFishy <69714456+Ianisop@users.noreply.github.com>
2025-05-18 15:14:01 +0200

    started restructuring

--- Makefile
+++ Makefile
@@ -1,10 +1,15 @@
+# Compiler
 CXX = g++ -g3
 
+# Paths
 VULKAN_SDK_PATH = /home/user/VulkanSDK/x.x.x.x/x86_64
 STB_INCLUDE_PATH = /home/user/libraries/stb
 TINYOBJ_INCLUDE_PATH = /home/user/libraries/tinyobjloader
+CORE_DIR = core
 IMGUI_DIR = imgui
 IMGUI_BACKENDS = $(IMGUI_DIR)/backends
+
+# Source files
 IMGUI_SRCS = \
 	$(IMGUI_DIR)/imgui.cpp \
 	$(IMGUI_DIR)/imgui_draw.cpp \
@@ -14,24 +19,35 @@ IMGUI_SRCS = \
 	$(IMGUI_BACKENDS)/imgui_impl_glfw.cpp \
 	$(IMGUI_BACKENDS)/imgui_impl_vulkan.cpp
 
-CFLAGS = -std=c++17 -I$(VULKAN_SDK_PATH)/include -I$(STB_INCLUDE_PATH) -I$(TINYOBJ_INCLUDE_PATH) -I$(IMGUI_DIR) -I$(IMGUI_BACKENDS)
-LDFLAGS = -lglfw -lvulkan -ldl -lpthread -lX11 -lXxf86vm -lXrandr -lXi
+CORE_SRCS = \
+	$(CORE_DIR)/renderer.cpp
 
+# Compiler flags
+CFLAGS = -std=c++17 \
+	-I$(VULKAN_SDK_PATH)/include \
+	-I$(STB_INCLUDE_PATH) \
+	-I$(TINYOBJ_INCLUDE_PATH) \
+	-I$(IMGUI_DIR) \
+	-I$(IMGUI_BACKENDS) \
+	-I$(CORE_DIR)
+
+# Linker flags
+LDFLAGS = -L$(VULKAN_SDK_PATH)/lib \
+	-lglfw -lvulkan -ldl -lpthread -lX11 -lXxf86vm -lXrandr -lXi
 
 # Shader compiler
 GLSLANG_VALIDATOR = glslangValidator
 
-# Shader source and compiled outputa
+# Shaders
 SHADERS = shaders/shader.vert shaders/shader.frag
 SPV_SHADERS = $(SHADERS:.vert=.vert.spv)
 SPV_SHADERS := $(SPV_SHADERS:.frag=.frag.spv)
 
 # Main target
-VulkanTest: main.cpp $(SPV_SHADERS) $(IMGUI_SRCS)
-	$(CXX) $(CFLAGS) -o VulkanTest main.cpp $(IMGUI_SRCS) $(LDFLAGS)
+VulkanTest: main.cpp $(CORE_SRCS) $(IMGUI_SRCS) $(SPV_SHADERS)
+	$(CXX) $(CFLAGS) -o $@ main.cpp $(CORE_SRCS) $(IMGUI_SRCS) $(LDFLAGS)
 
-
-# Shader compile rule
+# Shader compile rules
 %.vert.spv: %.vert
 	$(GLSLANG_VALIDATOR) -V $< -o $@
 
---
+++ core/renderer.h
@@ -0,0 +1,195 @@
+#pragma once
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/hash.hpp>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
+
+#define IMGUI_DEFINE_MATH_OPERATORS
+#include "imgui_impl_vulkan.h"
+#include "imgui_impl_glfw.h"
+#include "imgui.h"
+
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <algorithm>
+#include <chrono>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <cstdint>
+#include <limits>
+#include <array>
+#include <optional>
+#include <set>
+#include <unordered_map>
+
+namespace std
+{
+    template <>
+    struct hash<Vision::Vertex>
+    {
+        size_t operator()(Vision::Vertex const &vertex) const
+        {
+            return ((hash<glm::vec3>()(vertex.pos) ^ (hash<glm::vec3>()(vertex.color) << 1)) >> 1) ^ (hash<glm::vec2>()(vertex.texCoord) << 1);
+        }
+    };
+}
+
+namespace Vision
+{
+    struct QueueFamilyIndices
+    {
+        std::optional<uint32_t> graphicsFamily;
+        std::optional<uint32_t> presentFamily;
+
+        bool isComplete()
+        {
+            return graphicsFamily.has_value() && presentFamily.has_value();
+        }
+    };
+
+    struct SwapChainSupportDetails
+    {
+        VkSurfaceCapabilitiesKHR capabilities;
+        std::vector<VkSurfaceFormatKHR> formats;
+        std::vector<VkPresentModeKHR> presentModes;
+    };
+
+    struct UniformBufferObject
+    {
+        alignas(16) glm::mat4 model;
+        alignas(16) glm::mat4 view;
+        alignas(16) glm::mat4 proj;
+    };
+    struct Vertex
+    {
+        glm::vec3 pos;
+        glm::vec3 color;
+        glm::vec2 texCoord;
+
+        static VkVertexInputBindingDescription getBindingDescription()
+        {
+            VkVertexInputBindingDescription bindingDescription{};
+            bindingDescription.binding = 0;
+            bindingDescription.stride = sizeof(Vertex);
+            bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+            return bindingDescription;
+        }
+
+        static std::array<VkVertexInputAttributeDescription, 3> getAttributeDescriptions()
+        {
+            std::array<VkVertexInputAttributeDescription, 3> attributeDescriptions{};
+
+            attributeDescriptions[0].binding = 0;
+            attributeDescriptions[0].location = 0;
+            attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
+            attributeDescriptions[0].offset = offsetof(Vertex, pos);
+
+            attributeDescriptions[1].binding = 0;
+            attributeDescriptions[1].location = 1;
+            attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+            attributeDescriptions[1].offset = offsetof(Vertex, color);
+
+            attributeDescriptions[2].binding = 0;
+            attributeDescriptions[2].location = 2;
+            attributeDescriptions[2].format = VK_FORMAT_R32G32_SFLOAT;
+            attributeDescriptions[2].offset = offsetof(Vertex, texCoord);
+
+            return attributeDescriptions;
+        }
+
+        bool operator==(const Vertex &other) const
+        {
+            return pos == other.pos && color == other.color && texCoord == other.texCoord;
+        }
+    };
+
+    class Renderer
+    {
+    public:
+        void run();
+        bool loadObj(const char *path);
+    
+
+    private:
+        GLFWwindow *window;
+
+        VkInstance instance;
+        VkDebugUtilsMessengerEXT debugMessenger;
+        VkSurfaceKHR surface;
+
+        VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+        VkSampleCountFlagBits msaaSamples = VK_SAMPLE_COUNT_1_BIT;
+        VkDevice device;
+
+        VkQueue graphicsQueue;
+        VkQueue presentQueue;
+
+        VkSwapchainKHR swapChain;
+        std::vector<VkImage> swapChainImages;
+        VkFormat swapChainImageFormat;
+        VkExtent2D swapChainExtent;
+        std::vector<VkImageView> swapChainImageViews;
+        std::vector<VkFramebuffer> swapChainFramebuffers;
+
+        VkRenderPass renderPass;
+        VkDescriptorSetLayout descriptorSetLayout;
+        VkPipelineLayout pipelineLayout;
+        VkPipeline graphicsPipeline;
+
+        VkCommandPool commandPool;
+
+        VkImage colorImage;
+        VkDeviceMemory colorImageMemory;
+        VkImageView colorImageView;
+
+        VkImage depthImage;
+        VkDeviceMemory depthImageMemory;
+        VkImageView depthImageView;
+
+        uint32_t mipLevels;
+        VkImage textureImage;
+        VkDeviceMemory textureImageMemory;
+        VkImageView textureImageView;
+        VkSampler textureSampler;
+        u_int32_t minImageCount;
+        u_int32_t maxImageCount;
+
+        std::vector<Vertex> vertices;
+        std::vector<uint32_t> indices;
+        VkBuffer vertexBuffer;
+        VkDeviceMemory vertexBufferMemory;
+        VkBuffer indexBuffer;
+        VkDeviceMemory indexBufferMemory;
+
+        std::vector<VkBuffer> uniformBuffers;
+        std::vector<VkDeviceMemory> uniformBuffersMemory;
+        std::vector<void *> uniformBuffersMapped;
+
+        VkDescriptorPool descriptorPool;
+        VkDescriptorPool imGuiDescriptorPool;
+        std::vector<VkDescriptorSet> descriptorSets;
+
+        std::vector<VkCommandBuffer> commandBuffers;
+
+        std::vector<VkSemaphore> imageAvailableSemaphores;
+        std::vector<VkSemaphore> renderFinishedSemaphores;
+        std::vector<VkFence> inFlightFences;
+        uint32_t currentFrame = 0;
+
+        bool framebufferResized = false;
+    };
+}
